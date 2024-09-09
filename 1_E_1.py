7d31bccca7167a1b21fa441f947aefc903cc3f3d
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-09-09 17:14:06 -0500

    Uploading the whole file

---
+++ 1_E_1.py
@@ -0,0 +1,12 @@
+#Install Python and run a "Hello, World!" script. 
+
+print("Hello, World!")
+
+
+
+"""What You’ve Learned:
+You installed Python: Now your computer understands Python code.
+You wrote a script: Simple code that tells Python what to do.
+You ran it: Python executed your code and displayed the output.
+By running this, you’ve completed the basic process of writing and executing Python scripts!
+The rest of the learning comes by experimenting with other commands and expanding beyond just printing messages."""
---
+++ 1_E_2.py
@@ -0,0 +1,9 @@
+#Write a Python script that prints your name. 
+print("Jayaddep K")
+
+
+"""What You’ve Learned:
+You wrote a script: The script print("Jayadeep K") is a simple example of how to use Python to output text.
+You executed it: Running the script using Python displayed the text Jayadeep K on your screen.
+You understand the basics: This exercise introduced you to Python’s syntax and basic functions.
+By running this script, you've taken your first step in writing and executing Python code! Continue experimenting with different commands and expanding your knowledge to build more complex programs."""
