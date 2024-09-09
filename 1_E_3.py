0e43f47c61fc99cfa2ce6555f37a34dec0af0e68
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-09-09 17:35:56 -0500

    1. this Script is used to Find the version of python you are using.

---
+++ 1_E_3.py
@@ -0,0 +1,10 @@
+#Check the Python version installed on your system.
+"python3 --version"
+
+
+
+"""What You’ve Learned:
+You ran a command: The command python3 --version shows the installed Python 3 version on your system.
+You interpreted the output: The output tells you which version of Python 3 is available.
+By running this command, you’ve learned how to check the Python version, which is crucial for ensuring compatibility with scripts and libraries.
+Keep exploring other commands to get more familiar with your development environment!"""
