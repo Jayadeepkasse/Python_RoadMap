4bf0e552c79738d3df3a1f383dd78255659827b1
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-09-10 16:19:54 -0500

    This Script is to Display a Hello, World!

--- Topic_1/1_E_1.py
+++ Topic_1/1_E_1.py
@@ -4,7 +4,7 @@ print("Hello, World!")
 
 
 #What You’ve Learned:
-"""You installed Python: Now your computer understands Python code.
+""" You installed Python: Now your computer understands Python code.
 You wrote a script: Simple code that tells Python what to do.
 You ran it: Python executed your code and displayed the output.
 By running this, you’ve completed the basic process of writing and executing Python scripts!
