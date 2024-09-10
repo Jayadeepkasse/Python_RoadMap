98bc2c93ec447273d1fe362a144d56772364d853
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-09-10 16:25:33 -0500

    python Script that prints your name.

--- Topic_1/1_E_2.py
+++ Topic_1/1_E_2.py
@@ -2,7 +2,7 @@
 print("Jayaddep K")
 
 #What You’ve Learned:
-"""You wrote a script: The script print("Jayadeep K") is a simple example of how to use Python to output text.
+"""You wrote a script:The script print("Jayadeep K") is a simple example of how to use Python to output text.
 You executed it: Running the script using Python displayed the text Jayadeep K on your screen.
 You understand the basics: This exercise introduced you to Python’s syntax and basic functions.
 By running this script, you've taken your first step in writing and executing Python code! Continue experimenting with different commands and expanding your knowledge to build more complex programs."""
