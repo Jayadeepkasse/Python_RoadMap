c5a77a6a0636b0dff146f8b7ab187aaf2355c793
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-09-10 17:05:28 -0500

    script that prints the even numbers between 1 and 50.

---
+++ Topic_1/1_M_2.py
@@ -0,0 +1,8 @@
+#Write a script that prints the even numbers between 1 and 50.
+numbers=[x for x in range(1,50) if x%2==0]
+print(numbers)
+
+
+#1.take input range from user, 
+2.define a function,
+3. try except:ValueError
