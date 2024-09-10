9750a7cba6cffe642d8304011fc453206ebd1541
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-09-10 16:30:11 -0500

    script that prints 'welcome to python programing'

--- Topic_1/1_E_5.py
+++ Topic_1/1_E_5.py
@@ -2,7 +2,7 @@
 print("Welcome to Python programming!")
 
 
-"""What You’ve Learned:
+""" What You’ve Learned:
 You wrote a simple script: The print() function displays messages in the console.
 You ran the script: Executing this script outputs the message "Welcome to Python programming!".
 By running this, you’ve practiced how to print a message in Python. 
